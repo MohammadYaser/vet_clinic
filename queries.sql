f19dd60a7e60060e2d660a3c1cd9fd1194024dde
MohammadYaser <yasersafi19@outlook.com>
2023-10-24 22:56:14 +0430

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -3,3 +3,5 @@
 SELECT * FROM animals where name like '%mon';
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
+SELECT date_of_birth from animals WHERE name IN ('Agumon', 'Pikachu') ;
+
