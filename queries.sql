34fac18e53a0d7a065046324521a4c6120f01455
MohammadYaser <yasersafi19@outlook.com>
2023-10-24 22:56:34 +0430

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -4,4 +4,5 @@ SELECT * FROM animals where name like '%mon';
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
 SELECT date_of_birth from animals WHERE name IN ('Agumon', 'Pikachu') ;
+SELECT (name, escape_attempts) from animals WHERE weight_kg > 10.50;
 
